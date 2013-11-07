544d040d4c908a8874a2a99781baa3ce5ca5c590
kellimohr <kellimohr@gmail.com>
2013-11-06 20:16:39 -0800

    Added support for saving and picking of photos.

--- Camera/KMViewController.m
+++ Camera/KMViewController.m
@@ -8,7 +8,7 @@
 
 #import "KMViewController.h"
 
-@interface KMViewController ()
+@interface KMViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 - (void)showActionSheet:(id)sender; //Declare method to show action sheet
 
@@ -23,7 +23,7 @@
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     button.frame = CGRectMake(20.0f, 186.0f, 280.0f, 88.0f);
-    [button setTitle:@"Camera" forState:UIControlStateNormal];
+    [button setTitle:@"Get Image" forState:UIControlStateNormal];
     [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     button.tintColor = [UIColor darkGrayColor];
     [button addTarget:self action:@selector(showActionSheet:) forControlEvents:UIControlEventTouchUpInside];
@@ -33,18 +33,50 @@
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    [picker setDelegate:self];
+    
     //Get the name of the current pressed button
     NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
     if  ([buttonTitle isEqualToString:@"Delete"]) {
-        NSLog(@"Destructive pressed --> Delete Something");
+        NSLog(@"Delete pressed");
     }
     if ([buttonTitle isEqualToString:@"Album"]) {
         NSLog(@"Album pressed");
+        
+        [picker setSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum];
     }
     if ([buttonTitle isEqualToString:@"Camera"]) {
         NSLog(@"Camera pressed");
+        
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            [picker setSourceType:UIImagePickerControllerSourceTypeCamera];
+        } else {
+            [picker setSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum];
+        }
+        
+        [picker setAllowsEditing:YES];
+        [self presentViewController:picker animated:YES completion:^{
+            NSLog(@"Showing Camera");
+        }];
     }
 
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        UIImage *pickedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+        
+        // show the image to the user
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+        [imageView setImage:pickedImage];
+        [self.view addSubview:imageView];
+        
+        // save the image to the photos album
+        UIImageWriteToSavedPhotosAlbum(pickedImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    }];
+    
 }
 
 - (void)showActionSheet:(id)sender
@@ -61,6 +93,15 @@
                                   destructiveButtonTitle:destructiveTitle
                                   otherButtonTitles:album, camera, nil];
     [actionSheet showInView:self.view];
+}
+
+- (void)image: (UIImage *)image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo
+{
+    if (error) {
+        NSLog(@"Unable to save photo to camera roll");
+    } else {
+        NSLog(@"Saved Image To Camera Roll");
+    }
 }
 
 - (void)didReceiveMemoryWarning
