98f76ce846a202bbd5ad998baa44210753abff43
kellimohr <kellimohr@gmail.com>
2013-11-06 19:38:18 -0800

    Created UIACtionSheet for the aplication to choose between the Camera or the Photo Album.

--- Camera/KMViewController.h
+++ Camera/KMViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface KMViewController : UIViewController
+@interface KMViewController : UIViewController  <UIActionSheetDelegate>
 
 @end
--- Camera/KMViewController.m
+++ Camera/KMViewController.m
@@ -10,6 +10,8 @@
 
 @interface KMViewController ()
 
+- (void)showActionSheet:(id)sender; //Declare method to show action sheet
+
 @end
 
 @implementation KMViewController
@@ -18,6 +20,47 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(20.0f, 186.0f, 280.0f, 88.0f);
+    [button setTitle:@"Camera" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    button.tintColor = [UIColor darkGrayColor];
+    [button addTarget:self action:@selector(showActionSheet:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+    //Get the name of the current pressed button
+    NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
+    if  ([buttonTitle isEqualToString:@"Delete"]) {
+        NSLog(@"Destructive pressed --> Delete Something");
+    }
+    if ([buttonTitle isEqualToString:@"Album"]) {
+        NSLog(@"Album pressed");
+    }
+    if ([buttonTitle isEqualToString:@"Camera"]) {
+        NSLog(@"Camera pressed");
+    }
+
+}
+
+- (void)showActionSheet:(id)sender
+{
+    NSString *actionSheetTitle = @"Get Image"; //Action Sheet Title
+    NSString *camera = @"Camera";
+    NSString *album = @"Album";
+    NSString *destructiveTitle = @"Delete"; //Action Sheet Button Titles
+    NSString *cancelTitle = @"Cancel";
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:actionSheetTitle
+                                  delegate:self
+                                  cancelButtonTitle:cancelTitle
+                                  destructiveButtonTitle:destructiveTitle
+                                  otherButtonTitles:album, camera, nil];
+    [actionSheet showInView:self.view];
 }
 
 - (void)didReceiveMemoryWarning
